9a2dcf73c7d393f332d746ba54380823e0aed677
juaningan <juanjop@gmail.com>
2012-10-04 17:23:27 +0200

    Clean up module dependencies

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,12 +1,4 @@
 class uwsgi::install {
-  require uwsgi::install::repos
-  include user::uwsgi
-
-  Group <| title == uwsgi |>
-  User <| title == uwsgi |> {
-    before => Package ['uwsgi'],
-  }
-
   package {'uwsgi':
     ensure => installed
   }
