fd7e2d7fb453fced54ab4cecf54a1629434b3765
juaningan <juanjop@gmail.com>
2012-10-04 17:23:50 +0200

    Clean up module dependencies

--- manifests/install/repos.pp
+++
@@ -1,8 +0,0 @@
-class uwsgi::install::repos {
-  case $::operatingsystem {
-    redhat,centos,fedora,Scientific: {
-      require graphite::install::repos
-    }
-    default: { fail("${::hostname}: This module does not support operatingsystem ${::operatingsystem}") }
-  }
-}
