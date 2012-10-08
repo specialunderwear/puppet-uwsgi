1d95f725f1a1acd2313daa82d099b68522b84bea
juaningan <juanjop@gmail.com>
2012-10-08 09:20:50 +0200

    Added unsupported OS notify

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,6 +3,16 @@ class uwsgi::params {
   # Enable plugins
   $plugins = true
 
-  $package = ['uwsgi']
-  $plugins_package = ['uwsgi-plugin-rack-ruby1.8']
+  case $::operatingsystem {
+    Debian,Ubuntu: {
+      $configdir = '/etc/uwsgi/apps-enabled'
+      $package = ['uwsgi']
+      $plugins_package = ['uwsgi-plugin-rack-ruby1.8']
+    }
+    default: {
+      notify { "${module_name}_unsupported":
+        message => "The ${module_name} module is not supported on ${operatingsystem}",
+      }
+    }
+  }
 }
